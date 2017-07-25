57b4e4d31242ca259adfbf5cd536dad3855feba2
Vigilans <vigilans@foxmail.com>
2017-07-25 11:25:10 +0800

    Remove MDwiki index.html from repository. It will be downloaded locally.

--- restore_packages.bat
+++ restore_packages.bat
@@ -12,7 +12,14 @@ echo Restoring Client-side NPM Packages...
 cd ./HistoryContest.Client
 call npm install
 
+cd ../
+
+echo.
+echo Restoring MDWiki renderer html...
+cd ./HistoryContest.Wiki
+call powershell (new-object System.Net.WebClient).DownloadFile('http://dynalon.github.io/mdwiki/index.html', './index.html')
+echo Successfully downloaded index.html from http://dynalon.github.io/mdwiki/index.html.
+
 echo.
 echo Restore process finished.
 pause
-exit
