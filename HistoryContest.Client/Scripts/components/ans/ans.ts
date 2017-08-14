6f2772cf093327021a14d9ae7b70f0eb5359addf
Dailin <213160842@seu.edu.cn>
2017-08-14 20:49:29 +0800

    basic animation feature between pages completed.

--- HistoryContest.Client/Scripts/components/ans/ans.ts
+++ HistoryContest.Client/Scripts/components/ans/ans.ts
@@ -23,15 +23,225 @@ $(function(){
     set();//初始化
     answercard();
 	$('#start').click(function(){
-		alert("click start");
 		//$('#sec1').css("display","none");
 		$('#sec1').fadeOut(1000);
 		$('#sec2').fadeOut(1000);
+		$('#quiz-container').fadeOut(1000);
+		$('#banner').fadeOut(1000);
+		$('#submission').fadeOut(1000);
+
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q21').fadeIn(1000);
+		},1000);
+		/////////////////
+
+	});
+	$('#toQ2').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q1').fadeOut(1000);
+		$('#picq1').fadeOut(1000);
+
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q2').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ3').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q2').fadeOut(1000);
+		$('#picq2').fadeOut(1000);
+
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q3').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ4').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q3').fadeOut(1000);
+		$('#picq3').fadeOut(1000);
+
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q4').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ5').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q4').fadeOut(1000);
+		$('#picq4').fadeOut(1000);
+
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q5').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ6').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q5').fadeOut(1000);
+		$('#picq5').fadeOut(1000);
+
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q6').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ7').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q6').fadeOut(1000);
+		$('#picq6').fadeOut(1000);
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q7').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ8').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q7').fadeOut(1000);
+		$('#picq7').fadeOut(1000);
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q8').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ9').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q8').fadeOut(1000);
+		$('#picq8').fadeOut(1000);
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q9').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ10').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q9').fadeOut(1000);
+		$('#picq9').fadeOut(1000);
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q10').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ11').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q10').fadeOut(1000);
+		$('#picq10').fadeOut(1000);
 		
-		//$('#sec2').css("display","none");
-	})
-}
-)
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q11').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ12').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q11').fadeOut(1000);
+		$('#picq11').fadeOut(1000);
+		
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q12').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ13').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q12').fadeOut(1000);
+		$('#picq12').fadeOut(1000);
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q13').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ14').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q13').fadeOut(1000);
+		$('#picq13').fadeOut(1000);
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q14').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ15').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q14').fadeOut(1000);
+		$('#picq14').fadeOut(1000);
+		
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q15').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ16').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q15').fadeOut(1000);
+		$('#picq15').fadeOut(1000);
+		
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q16').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ17').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q16').fadeOut(1000);
+		$('#picq16').fadeOut(1000);
+		
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q17').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ18').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q17').fadeOut(1000);
+		$('#picq17').fadeOut(1000);
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q18').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ19').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q18').fadeOut(1000);
+		$('#picq18').fadeOut(1000);
+		
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q19').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ20').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q19').fadeOut(1000);
+		$('#picq19').fadeOut(1000);
+		
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q20').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ21').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q20').fadeOut(1000);
+		$('#picq20').fadeOut(1000);
+		
+		setTimeout(function(){
+			$('#quiz-container').fadeIn(1000);
+			$('#q21').fadeIn(1000);
+		},1000);
+	});
+	$('#toQ22').click(function(){
+		$('#quiz-container').fadeOut(1000);
+		$('#q21').fadeOut(1000);
+		$('#picq21').fadeOut(1000);
+		setTimeout(function(){
+			//$('#quiz-container').fadeIn(1000);
+			$('#submission').fadeIn(1000);
+		},1000);
+	});
+	
+});
+
 new Vue({
 	el:"#ans",
 	data:{
--- HistoryContest.Client/Scripts/components/ans/questions.js
+++ HistoryContest.Client/Scripts/components/ans/questions.js
@@ -45,43 +45,43 @@ var content = '',
 exports.set = function(){
     //生成多选题html
     for (questionsIteratorIndex = 0; questionsIteratorIndex < init.questions.length; questionsIteratorIndex++) {
-        content += '<section class="panel"><div class="inner columns" id="q'+(questionsIteratorIndex + 1)+'">'//题目id，为网页内链接表示（实现按钮跳转到下一题）
+        content += '<section class="panel" id="q'+(questionsIteratorIndex + 1)+'"><div class="inner columns" >'//题目id，为网页内链接表示（实现按钮跳转到下一题）
         +'<div class="intro joined"><h1>'+(questionsIteratorIndex + 1)+'</h1></div>'//网页上显示的题目序号
         +'<div class="span-3-25"><h3 class="major">'+ init.questions[questionsIteratorIndex].question+'</h3>';//问题内容
         for (answersIteratorIndex = 0; answersIteratorIndex < 4; answersIteratorIndex++) {//四个选项
             content += '<div class="field quarter"><input type="radio"'
             +' id="choice'+(questionsIteratorIndex + 1)+(answersIteratorIndex+1)//选项id，该id名称应与后端协商确定，与提交表单有关
             +'" value="'+answersIteratorIndex
-            +'" onclick="saveAns(this)"'
+            +'" <!--onclick="saveAns(this)-->'
             +'" name="question'+(questionsIteratorIndex + 1)+'" class="color2" />'//单选radio的name，同上
             +'<label for="choice'+(questionsIteratorIndex + 1)+(answersIteratorIndex+1)+'">'//同上 关于for属性可以参见参考文档
             +init.questions[questionsIteratorIndex].answers[answersIteratorIndex] +'</label></div><br>';//选项内容
         }
         content += '</div>';
     //    if(questionsIteratorIndex+1!= init.questions.length){
-            content += '<div class="intro  joined"><ul class="actions"><li><a href="#q'+(questionsIteratorIndex + 2)+'" class="button  color1 circle icon fa-angle-right">Next</a></li></ul></div></div></section>';        
-            content+='<section class="panel spotlight large right" ><div class="image  tinted" data-position="top left"><img src="/dist/Images/background'+(questionsIteratorIndex%9)+'.jpg" alt="" /></div></section>'
+            content += '<div class="intro  joined"><ul class="actions"><li><a id="toQ'+(questionsIteratorIndex + 2)+'" class="button  color1 circle icon fa-angle-right">Next</a></li></ul></div></div></section>';        
+            content+='<section id="picq'+(questionsIteratorIndex + 1)+'" class="panel spotlight large right" ><div class="image  tinted" data-position="top left"><img src="/dist/Images/background'+(questionsIteratorIndex%9)+'.jpg" alt="" /></div></section>'
     //    }
     }
 
     //生成判断题html
      for (; questionsIteratorIndex < init.questions.length+init.TFs.length; questionsIteratorIndex++) {//延续之前的索引
-        content += '<section class="panel"><div class="inner columns" id="q'+(questionsIteratorIndex + 1)+'">'
+        content += '<section class="panel" id="q'+(questionsIteratorIndex + 1)+'"><div class="inner columns">'
         +'<div class="intro joined"><h1>'+(questionsIteratorIndex + 1)+'</h1></div>'
         +'<div class="span-3-25"><h3 class="major">'+ init.questions[questionsIteratorIndex-init.TFs.length].question+'</h3>';
         for (answersIteratorIndex = 0; answersIteratorIndex < 2; answersIteratorIndex++) {//两个选项
             content += '<div class="field quarter"><input type="radio" id="choice'+(questionsIteratorIndex + 1)+(answersIteratorIndex+1)
             +'" value="'+answersIteratorIndex
-            +'" onclick="saveAns(this)"'
+            +'" <!--onclick="saveAns(this)-->'
             +'" name="question'+(questionsIteratorIndex + 1)+'" class="color2" />'
             +'<label for="choice'+(questionsIteratorIndex + 1)+(answersIteratorIndex+1)+'">'
             +(answersIteratorIndex==0?'正确':'错误') +'</label></div><br>';//判断题，选项为正确或错误
         }
         content += '</div>';
         if(questionsIteratorIndex+1!= init.questions.length+init.TFs.length){//如果是最后一题，那么不需要有下一页按钮
-            content += '<div class="intro  joined"><ul class="actions"><li><a href="#q'+(questionsIteratorIndex + 2)+'" class="button  color1 circle icon fa-angle-right">Next</a></li></ul></div></div></section>'; 
+            content += '<div class="intro  joined"><ul class="actions"><li><a id="toQ'+(questionsIteratorIndex + 2)+'" class="button  color1 circle icon fa-angle-right">Next</a></li></ul></div></div></section>'; 
                
-        content+='<section class="panel spotlight large right" ><div class="image  tinted" data-position="top left"><img src="/dist/Images/background'+(questionsIteratorIndex %9)+'.jpg" alt="" /></div></section>'
+        content+='<section  id="picq'+(questionsIteratorIndex + 1)+'" class="panel spotlight large right" ><div class="image  tinted" data-position="top left"><img src="dist/Images/background'+(questionsIteratorIndex %9)+'.jpg" alt="" /></div></section>'
         }
         else{
              content += '<div class="intro  joined"><ul class="actions"><li><a href="#submission" class="button  color1 circle icon fa-angle-right">Next</a></li></ul></div></div></section>'; 
@@ -97,7 +97,7 @@ var contentFooter='';
 exports.answercard= function(){
     
     for(var i=0;i<init.questions.length+init.TFs.length;i++){
-        contentFooter +='<a href="#q'+(i+1)+'" id="question'+(i+1)+'" class="questionId">'+(i+1)+"</a>";
+        contentFooter +='<a id="jumptoq'+(i+1)+'" id="question'+(i+1)+'" class="questionId">'+(i+1)+"</a>";
 
     }
     $("#answerCard").html(contentFooter);
