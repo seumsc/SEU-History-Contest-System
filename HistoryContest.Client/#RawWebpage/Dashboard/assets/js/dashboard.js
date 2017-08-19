876353b5c2698e8b9995dd8c773b0520c2979b04
AlbertSerica <943914044@qq.com>
2017-08-19 10:16:57 +0800

    new features for dashboard
    adding: fuzzy search
    optimized: more dynamic&structure adjustment

--- HistoryContest.Client/#RawWebpage/Dashboard/assets/js/dashboard.js
+++ HistoryContest.Client/#RawWebpage/Dashboard/assets/js/dashboard.js
@@ -1,8 +1,34 @@
+var schoolInfo={
+    "DepartmentID":711,
+    "CounselorName":"郭佳",
+    "MaxScore":91,
+    "AverageScore":75.21,
+    "ScoreBandCount":{
+        "HigherThan90":5,
+        "HigherThan75":36,
+        "HigherThan60":100,
+        "Failed":20
+    }
+}
+var DepartmentNameMap={
+    "010":"建筑学院",
+    "020":"机械工程学院",
+    "030":"能源与环境学院",
+    "040":"信息科学与工程学院",
+    "050":"土木工程学院",
+    "060":"电子科学与工程学院",
+    "070":"数学学院",
+    "080":"自动化学院",
+    "090":"计算机科学与工程学院、软件学院",
+    "711":"计算机科学与工程学院、软件学院"
+}
+
 var undo={
     'students':[
-        {"ID":"09016435","Name":"杨航源","CardID":"213161269"},
-        {"ID":"09016423","Name":"陈启宣","CardID":"213161299"},
-        {"ID":"09016414","Name":"罗崟洪","CardID":"213163210"}
+        {"ID":"09016435","Name":"杨航源","Score":60,"CardID":"213161269"},
+        {"ID":"09016423","Name":"陈启宣","Score":100,"CardID":"213161299"},
+        {"ID":"09016414","Name":"罗崟洪","Score":0,"CardID":"213163210"}
+       
     ]
 };
 var done={
@@ -12,25 +38,42 @@ var done={
         {"ID":"09016414","Name":"罗崟洪","Score":0,"CardID":"213163210"}
         ]
 };
+var config={
+    generalInfo:schoolInfo,
+    doneNumber:done.students.length,
+    undoNumber:undo.students.length,
+    total:done.students.length+undo.students.length,
+    comments:{
+        dones:{
+            perfect:"大家都很听话，全部完成作答了哦！",
+            common:done.students.length+"人已完成答题,"         
+        },
+        undos:{
+            worst:"偌大的学院，到现在还没有一人完成，大家都去哪儿浪了呢？",
+            common:"仍有"+undo.students.length+"人未完成。"
+        }
+    }
+
+}
 var by = function(name){
     return function(o, p){
-      var a, b;
-      if (typeof o === "object" && typeof p === "object" && o && p) {
-        a = o[name];
-        b = p[name];
-        if (a === b) {
-          return 0;
+        var a, b;
+        if (typeof o === "object" && typeof p === "object" && o && p) {
+            a = o[name];
+            b = p[name];
+            if (a === b) {
+            return 0;
+            }
+            if (typeof a === typeof b) {
+            return a < b ? 1 : -1;
+            }
+            return typeof a < typeof b ? 1 : -1;
         }
-        if (typeof a === typeof b) {
-          return a < b ? 1 : -1;
+        else {
+            throw ("error");
         }
-        return typeof a < typeof b ? 1 : -1;
-      }
-      else {
-        throw ("error");
-      }
     }
-   }
+}
    
 function setUndo(UNDO){
     var undoContent=""       
@@ -54,21 +97,57 @@ function setDone(DONE){
     $("#table-done").find("tbody").html(doneContent);
 }
 function initChartist(){ 
-    Chartist.Pie('#chartPreferences', {
-    labels: ['62%','38%'],
-    series: [50, 50]
+    var labelForDone=Math.round(100*(config.doneNumber/config.total))+"%";
+        labelForUndo=Math.round(100*(config.undoNumber/config.total))+"%";
+        //以下总人数还没有和上面的数据统一
+        labelA=Math.round(100*(config.generalInfo.ScoreBandCount.HigherThan90/161))+"%";
+        labelB=Math.round(100*(config.generalInfo.ScoreBandCount.Failed/161))+"%";
+        labelC=Math.round(100*(config.generalInfo.ScoreBandCount.HigherThan75/161))+"%";
+        labelD=Math.round(100*(config.generalInfo.ScoreBandCount.HigherThan60/161))+"%";
+    
+    Chartist.Pie('#completion-chart', {
+       
+    labels: [labelForDone,
+            (config.undoNumber==0?'':labelForUndo)],
+    series: [config.doneNumber, config.undoNumber]
     });   
-    Chartist.Pie('#chartOverallPreferences', {
-    labels: ['2%','80%',"10%","8%"],
-    series: [2, 80,10,8]
+    Chartist.Pie('#overall-chart', {
+    labels: [labelA,labelB,labelC,labelD],
+    series: [config.generalInfo.ScoreBandCount.HigherThan90,
+        config.generalInfo.ScoreBandCount.Failed,
+        config.generalInfo.ScoreBandCount.HigherThan75,
+        config.generalInfo.ScoreBandCount.HigherThan60
+    ]
     });   
 }
 
+function commonSet(){
+    $("#school-name").html(DepartmentNameMap[config.generalInfo.DepartmentID]);
+    if(config.undoNumber==0){
+        $("#done-info").html(config.comments.dones.perfect);
+        $("#undo-info").hide();
+        $("#empty-comment").show();
+    }
+    else if(config.doneNumber==0){
+        $("#done-info").hide();
+        $("#undo-info").html(config.comments.undos.worst);
+        $("#empty-comment").hide();
+    }
+    else{
+        $("#done-info").html(config.comments.dones.common);
+        $("#undo-info").html(config.comments.undos.common);
+        $("#empty-comment").hide();
+    }
+    $("#average-score").html(config.generalInfo.AverageScore);
+
+}
 var temp=$.extend(true,done);//深拷贝 !important
 var cnt=0;
 $(function () {
     setUndo(undo);
     setDone(done);
+    initChartist();
+    commonSet();
     console.log(done);
     $("#table-done").find("th.score").click((function(){
        if(cnt%3==0){
@@ -117,11 +196,11 @@ $(function () {
          $('#table-done table tbody tr').hide().filter(":contains('"+$key+"')").show();
      });
 
-     initChartist()
+     
 
  $.notify({
      icon: 'glyphicon glyphicon-heart-empty',
-     message: "你好，XXX，欢迎来到校史校情知识竞赛管理系统"
+     message: "您好，"+schoolInfo.CounselorName+"，欢迎来到校史校情知识竞赛管理系统"
 
  }, {
      type: 'info',
