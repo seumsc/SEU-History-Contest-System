bc951c987d449a87a2e581ed00bdeba2ac6dd247
AlbertSerica <943914044@qq.com>
2017-08-19 10:20:11 +0800

    introducing result.html
    features: read DevLog#0818 for reference

---
+++ HistoryContest.Client/#RawWebpage/Result/assets/js/result.js
@@ -0,0 +1,149 @@
+var init={'questions':[{'question':'南京高等师范学校继三所师范学校成为我国成立的最早的第四所国立高等师范学校，其中不是这三所学校之一的是：','answers':['武昌高师','北京高师','长沙高师','广州高师'],'correctAnswer':3},
+{'question':'江谦认为国家的富强有赖于科学、实业的发达，先后增设了三个专修科，其中哪一个不属于这三科之一？','answers':['农业','工业','商业','教育业'],'correctAnswer':4},
+{'question':'下面哪一项不属于郭秉文的“自动主义”？','answers':['学习上的自学和自力研究','思想上的自律和自强','生活上的自立和自理','学术、文化、体育活动的自行组织和主办'],'correctAnswer':2},
+{'question':'下面哪一项不是南京高等师范学院办学者的宗旨（南京高等师范学院教育的特色）？','answers':['倡导学生的爱国精神，为国家培养科学人才','调和文理，沟通中西，积极引进国外的先进科学技术','用科学的精神办教育，用科学的方法育才','促进教育、科学的共同发展，促进科学人才的成长和脱颖而出'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1},
+{'question':'我是问题','answers':['我是答案','我是选项','我是选项','我是选项'],'correctAnswer':1}],
+'TFs':[{'question':'三江师范时期的速成科学制是1年。','correctAnswer':2},
+{'question':'三江师范时期的速成科学制是1年。','correctAnswer':2},
+{'question':'三江师范时期的速成科学制是1年。','correctAnswer':2},
+{'question':'三江师范时期的速成科学制是1年。','correctAnswer':2},
+{'question':'三江师范时期的速成科学制是1年。','correctAnswer':2},
+{'question':'三江师范时期的速成科学制是1年。','correctAnswer':2},
+{'question':'三江师范时期的速成科学制是1年。','correctAnswer':2},
+{'question':'三江师范时期的速成科学制是1年。','correctAnswer':2},
+{'question':'三江师范时期的速成科学制是1年。','correctAnswer':2},
+{'question':'三江师范时期的速成科学制是1年。','correctAnswer':2}]
+};
+
+var result={   
+    "score":80,
+    "details":[
+        {"ID":0,"rightAnswer":1,"submittedAnswer":1,"isCorrect":true},
+        {"ID":1,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false},
+        {"ID":2,"rightAnswer":1,"submittedAnswer":2,"isCorrect":false},
+        {"ID":3,"rightAnswer":1,"submittedAnswer":1,"isCorrect":true},
+        {"ID":4,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false},
+        {"ID":5,"rightAnswer":1,"submittedAnswer":2,"isCorrect":false},
+        {"ID":6,"rightAnswer":1,"submittedAnswer":1,"isCorrect":true},
+        {"ID":7,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false},
+        {"ID":8,"rightAnswer":1,"submittedAnswer":2,"isCorrect":false},
+        {"ID":9,"rightAnswer":1,"submittedAnswer":1,"isCorrect":true},
+        {"ID":10,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false},
+        {"ID":11,"rightAnswer":1,"submittedAnswer":2,"isCorrect":false},
+        {"ID":12,"rightAnswer":1,"submittedAnswer":1,"isCorrect":true},
+        {"ID":13,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false},
+        {"ID":14,"rightAnswer":1,"submittedAnswer":2,"isCorrect":false},
+        {"ID":15,"rightAnswer":1,"submittedAnswer":1,"isCorrect":true},
+        {"ID":16,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false},
+        {"ID":17,"rightAnswer":1,"submittedAnswer":2,"isCorrect":false},
+        {"ID":18,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false},
+        {"ID":19,"rightAnswer":1,"submittedAnswer":2,"isCorrect":false},
+        {"ID":20,"rightAnswer":1,"submittedAnswer":1,"isCorrect":true},
+        {"ID":21,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false},
+        {"ID":22,"rightAnswer":1,"submittedAnswer":1,"isCorrect":true},
+        {"ID":23,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false},
+        {"ID":24,"rightAnswer":1,"submittedAnswer":1,"isCorrect":true},
+        {"ID":25,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false},
+        {"ID":26,"rightAnswer":1,"submittedAnswer":1,"isCorrect":true},
+        {"ID":27,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false},
+        {"ID":28,"rightAnswer":1,"submittedAnswer":1,"isCorrect":true},
+        {"ID":29,"rightAnswer":1,"submittedAnswer":0,"isCorrect":false}
+    ]
+}
+
+var tableContent="";
+function set(){
+    for(var resultsIteratorIndex=0;resultsIteratorIndex<result.details.length;resultsIteratorIndex++){
+        if(resultsIteratorIndex%5==0)
+            tableContent+="<tr>";
+        tableContent+='<td><span class="num">'+(resultsIteratorIndex+1)+' </span>'
+        if(result.details[resultsIteratorIndex].isCorrect){
+            tableContent+=' <span class="fa fa-check" style="color:#3caa00"></span></td>'
+        }
+        else{
+            tableContent+='  <span class="fa fa-close" style="color:rgb(240,130,0)"></span></td>'
+        }
+                        
+        if(resultsIteratorIndex%5==4)
+            tableContent+="</tr>";
+    }
+    $('#table-content').html(tableContent);
+    $("#score").text(result.score);
+}
+$(function(){
+
+    set();
+    $("td").hover(function(event) {
+        var reviewContent="";
+    
+        var $tgt=$(event.target);
+        var questionNum=$tgt.find(".num").text();
+        var rightAns=result.details[questionNum-1].rightAnswer;
+        var submittedAns=result.details[questionNum-1].submittedAnswer;
+        var isCorrect=result.details[questionNum-1].isCorrect;
+        if (questionNum<=20){
+            reviewContent+='<h3 class="major">' +questionNum+'. '+init.questions[questionNum-1].question+'</h3>';
+            for(answersIteratorIndex = 0; answersIteratorIndex < 4; answersIteratorIndex++ ){
+                if(answersIteratorIndex==rightAns){
+                    reviewContent += '<div class="field quarter" style="color:#3caa00">答案：'
+                    
+                    +init.questions [questionNum-1].answers[answersIteratorIndex] +'</div><br>';//选项内容
+                }
+                else if(answersIteratorIndex==submittedAns && !isCorrect){
+                    reviewContent += '<div class="field quarter" style="color:rgb(240,130,0)">'
+                    
+                    +init.questions [questionNum-1].answers[answersIteratorIndex] +' <span class="fa fa-close" style="color:rgb(240,130,0)"></span></div><br>';//选项内容
+                }else{
+                reviewContent += '<div class="field quarter">'
+                
+                +init.questions [questionNum-1].answers[answersIteratorIndex] +'</div><br>';//选项内容
+                }
+            }
+
+        }
+        else{
+            reviewContent+='<h3 class="major">' +questionNum+'. '+init.TFs[questionNum-21].question+'</h3>';
+            for(answersIteratorIndex = 0; answersIteratorIndex < 2; answersIteratorIndex++ ){
+                if(answersIteratorIndex==rightAns){
+                    reviewContent += '<div class="field quarter" style="color:#3caa00">答案：'
+                    
+                    +(answersIteratorIndex==0?'正确':'错误')  +'</div><br>';//选项内容
+                }
+                else if(answersIteratorIndex==submittedAns && !isCorrect){
+                    reviewContent += '<div class="field quarter" style="color:rgb(240,130,0)">'
+                    
+                    +(answersIteratorIndex==0?'正确':'错误')  +' <span class="fa fa-close" style="color:rgb(240,130,0)"></span></div><br>';//选项内容
+                }else{
+                reviewContent += '<div class="field quarter">'
+                
+                +(answersIteratorIndex==0?'正确':'错误')  +'</div><br>';//选项内容
+                }
+
+              
+            
+            }
+
+        }
+        $("#review-container").html(reviewContent);
+       
+        
+        $("#review-container").show();//hover后显示题目
+    },function() {
+        $("#review-container").hide();//hover后显示题目
+    });
+})
