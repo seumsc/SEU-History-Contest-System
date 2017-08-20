8b34e74acaaddccecc71c01da5161261261c7647
Vigilans <vigilans@foxmail.com>
2017-08-21 00:20:33 +0800

    fix publish.bat bug

--- publish.bat
+++ publish.bat
@@ -1,7 +1,17 @@
 @echo off
-call dotnet restore "./HistoryContest.sln"
-call dotnet publish "./HistoryContest.Server/HistoryContest.Server.csproj" --output "./HistoryContest.Site" --configuration Release
-cd ./HistoryContest.Site
+if exist HistoryContest.Site (
+    del /q /s .\HistoryContest.Site\*.* > nul
+) else (
+    mkdir HistoryContest.Site
+)
+
+cd ./HistoryContest.Server
+call dotnet restore
+if %ERRORLEVEL% NEQ 0 exit
+call dotnet publish --output "../HistoryContest.Site" --configuration Release
+if %ERRORLEVEL% NEQ 0 exit
+
+cd ../HistoryContest.Site
 echo @echo off > run_app.bat
 echo call dotnet HistoryContest.Server.dll -rb -env Production >> run_app.bat
 call run_app.bat
