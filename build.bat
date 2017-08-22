5d412019f182663d60fca38451207bd057ea0998
Vigilans <vigilans@foxmail.com>
2017-08-22 17:48:30 +0800

    improve the behavior of build.bat in wiki installation

--- build.bat
+++ build.bat
@@ -36,7 +36,7 @@ cd ../
 echo.
 echo (%i%/%n%)Restoring MDWiki renderer html...
 cd ./HistoryContest.Wiki
-call powershell -command "(new-object System.Net.WebClient).DownloadFile('http://dynalon.github.io/mdwiki/index.html', './index.html')"
+if not exist ./index.html call powershell -command "(new-object System.Net.WebClient).DownloadFile('http://dynalon.github.io/mdwiki/index.html', './index.html')"
 echo Successfully downloaded index.html from http://dynalon.github.io/mdwiki/index.html.
 set /a i=i+1
 
