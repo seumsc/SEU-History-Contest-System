4426910f8bd4b5ab2a297b48aa685c335f1b7ebb
Vigilans <vigilans@foxmail.com>
2017-08-23 11:56:57 +0800

    ready for remote debug

--- HistoryContest.Server/Data/Migrations/migrations_new.bat
+++ HistoryContest.Server/Data/Migrations/migrations_new.bat
@@ -3,7 +3,6 @@ if "%1"=="" goto ArgumentNull
 cd ../../
 call dotnet ef migrations add %1 --output-dir ./Data/Migrations
 call dotnet ef database update
-exit
 
 :ArgumentNull
 echo Please pass an argument to specify the name of the new migration.
