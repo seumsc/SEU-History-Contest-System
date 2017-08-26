cd5d110208880724c95a91a750b188c2cbea00c4
Dailin Hu <213160842@seu.edu.cn>
2017-08-26 11:22:43 +0800

    slaughtered bug no.3: id adjusted according to api

--- HistoryContest.Client/Scripts/components/ans/ans.js
+++ HistoryContest.Client/Scripts/components/ans/ans.js
@@ -1,40 +1,58 @@
 var $ = require("../../../node_modules/jquery/dist/jquery.min.js");
 var inTime = require('./ans.ts').inTime;
-	var answerQues = new Array(30);
-	answerQues[0] = "curAns"
-	exports.saveAns=function(clickID){
-		var ans = clickID.value;
-		var id=clickID;
-		var ID = (id.length == 8 ? parseInt(id[6]) : parseInt(id[6])*10+parseInt(id[7]));
-		var testing;
-		var ans = parseInt(id[id.length-1])-1;
-		var check ={};						
-		check.ID=ID;
-		check.answer=ans;
-		//answerQues.push(check);//用push方法传入数组
-		answerQues[ID-1] = check;
-		testing=JSON.stringify(answerQues);
-		console.log(testing);
-		$("#question"+ID).addClass("answered");
-		setTimeout(function(){
-			$("#question"+ID).click();
-		},300);
+var answerQues = new Array(30);
+// answerQues[0] = "curAns"
+exports.saveAns = function (clickID) {
+	var ans = clickID.value;
+	var id = clickID;
+	var ID = (id.length == 8 ? parseInt(id[6]) : parseInt(id[6]) * 10 + parseInt(id[7]));
+	var testing;
+	var ans = parseInt(id[id.length - 1]) - 1;
+	var check = {};
+	check.id= ID;
+	check.answer = ans;
+	//answerQues.push(check);//用push方法传入数组
+	answerQues[ID - 1] = check;
+	testing = JSON.stringify(answerQues);
+	console.log(testing);
+	$("#question" + ID).addClass("answered");
+	setTimeout(function () {
+		$("#question" + ID).click();
+	}, 300);
+}
+exports.saveAnsID = function(questions){
+	// alert(questions);
+	for(var i = 0;i<30; i++){
+		answerQues[i].id = questions[i].id;
+		// alert(JSON.stringify(questions[i]));
+		// alert(JSON.stringify(answerQues[i]));
 	}
-	exports.submit=function(inTime){
-		var tot = 0;
-		for(var i = 0; i<30; i++)
-			if(answerQues[i] == null) tot++;
-		if(tot!=0 && inTime)
-			alert("您还有"+tot+"题未作答题目哦!");
-		else {
-			alert("已提交！");
-		/*	$.ajax(
-				{ 
-					'url':'login.php',//第一个参数url
-					'data':testing,
-					'success':function(data){//success表示，当服务器返回数据成功后，该做什么，返回的数据储存在data中，我们直接把data传入函数中。
-				}, 
-				'type':'post',//type是ajax的方法，可以用post可以用get，两种方法的区别可以自己查阅资料 
-				'dataType':'json',//传递的数据类型，对应我上面的数据格式，这里用json。数据类型也可以是html/xml等 });*/
-		}
+}
+exports.submit = function (inTime) {
+	var tot = 0;
+	for (var i = 0; i < 30; i++)
+		if (answerQues[i] == null) tot++;
+	if (tot != 0 && inTime)
+		alert("您还有" + tot + "题未作答题目哦!");
+	else {
+		alert(JSON.stringify(answerQues));
+		$.ajax({
+			url: '/api/Result', //请求的url地址
+			type: "POST", //请求方式
+			dataType: "json", //返回格式为json
+			async: false,
+			data: JSON.stringify(answerQues),
+			contentType: "application/json",
+			beforeSend: function () {
+			},
+			success: function (req) {
+				alert(JSON.stringify(req));
+			},
+			complete: function () {
+			},
+			error: function (request) {
+				alert("error:" + JSON.stringify(request));
+			}
+		});
 	}
+}
---
+++ HistoryContest.Client/Scripts/components/ans/result/ansresult.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';//不能被注释掉！
+import jQuery from 'jquery';
+var $ = jQuery.noConflict();
+
+
+export default {
+	data() {
+		return {
+		}
+	},
+	mounted() {
+    },
+    methods:{
+        showResult:function(){        
+        }
+    }
+}
