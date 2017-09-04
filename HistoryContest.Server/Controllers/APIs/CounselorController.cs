72062ce04980eeaf66ceb32ef3ccb654f91d66c3
Vigilans <vigilans@foxmail.com>
2017-09-05 07:33:03 +0800

    reopen antiforgery validation

--- HistoryContest.Server/Controllers/APIs/CounselorController.cs
+++ HistoryContest.Server/Controllers/APIs/CounselorController.cs
@@ -16,7 +16,7 @@ using HistoryContest.Server.Extensions;
 namespace HistoryContest.Server.Controllers.APIs
 {
     [Authorize(Roles = "Counselor, Administrator")]
-    //[ValidateAntiForgeryToken]
+    [ValidateAntiForgeryToken]
     [Produces("application/json")]
     [Route("api/[controller]")]
     public class CounselorController : Controller
