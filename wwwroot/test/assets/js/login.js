1b865549a6f1cb4803cffda6757351acf90b3a82
AlbertSerica <943914044@qq.com>
2017-09-07 19:52:57 +0800

    bug fix

--- wwwroot/test/assets/js/login.js
+++ wwwroot/test/assets/js/login.js
@@ -87,27 +87,80 @@ $(function () {
         var usn = $(this.userName).val();
         var pwd = $(this.password).val();
         var rnm = $(this.realName).val();
-        var dt = {
+        var rdt = {
             "userName": usn,
             "password": pwd,
             "realName":rnm,
             "role":"Student"
         };
-        console.log(dt);
+        console.log(rdt);
         $.ajax({
             url: "/api/Account/Register",
             type: "POST", 
             async: true, 
             contentType: "application/json-patch+json",
             //data
-            data: JSON.stringify(dt), 
+            data: JSON.stringify(rdt), 
             dataType: "json", //返回格式为json
             success: function (req) {
                 console.log(req);
                 if(req.isSuccessful){
                     $("#register-message").removeClass().addClass("text-success")
                     $("#register-message").text("注册成功，即将进入竞赛系统...").fadeIn(); 
-                    setTimeout(window.location.href="index.html",1000);       
+                    
+                    setTimeout(function(){
+                        var usn = rdt.userName;
+                        var pwd =rdt.password;
+                        var dt = {
+                            "userName": usn,
+                            "password": pwd
+                        };
+                        $.ajax({
+                            url: '/api/Account/Login', //请求的url地址
+                            type: "POST", //请求方式
+                            async: true, //请求是否异步，默认为异步，这也是ajax重要特性
+                            crossDomain: true,
+                            contentType: "application/json-patch+json",
+                            //data
+                            data: JSON.stringify(dt), //参数值
+                            dataType: "json", //返回格式为json
+                            success: function (req,status,xhr) {
+                                if (req.isSuccessful) {
+                                    if (req.userViewModel.role == "Student") {
+                                        window.location.href = "index.html";
+                                    } else if (req.userViewModel.role == "Administrator" ||
+                                        req.userViewModel.role == "Counselor") {
+                                       window.location.href = "dashboard.html";
+                                    } else {
+                                        console.log(req.role);
+                                    }
+                                } 
+                                else {
+                                    if (req.message == "User already logged in") {
+                                        $("#common-error").text("当前用户已登录")
+                                            .fadeIn(setTimeout(function () {
+                                                $("#common-error").fadeOut();
+                                            }, 2000));
+                                        ///////logout     !!!delete before dist !danger
+                                       logout();
+                                    } 
+                                    else {
+                                        $("#common-error").text("用户名或密码错误，或尚未注册账号")
+                                            .fadeIn(setTimeout(function () {
+                                                $("#common-error").fadeOut();
+                                            }, 2000));
+                                    }
+                                }
+                            },
+                            error: function () {
+                                //请求出错处理
+                                $("#common-error").text("登录失败,请检查网络是否通畅")
+                                    .fadeIn(setTimeout(function () {
+                                        $("#common-error").fadeOut();
+                                    }, 2000));
+                            }
+                        });
+                    },1000);       
                 }
                 else{
                     $("#register-message").removeClass().addClass("text-danger");
